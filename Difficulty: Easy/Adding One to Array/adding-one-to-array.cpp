0b0fc03413925d2a75cc55bc3990559fdaceb8b7
SHIVAM KESHARI <149083540+SHIVAMKESHARI23@users.noreply.github.com>
2026-05-05 23:02:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Adding One to Array/adding-one-to-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    vector<int> addOne(vector<int>& arr) {
+        // code here
+        for(int i=arr.size()-1;i>=0;i--){
+            if(arr[i]<9){
+                arr[i]++;
+                return arr;
+            }
+            arr[i]=0;
+        }
+        arr.insert(arr.begin(),1);
+        return arr;
+    }
+};
