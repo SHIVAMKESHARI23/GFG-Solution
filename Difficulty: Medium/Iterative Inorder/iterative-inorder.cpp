245e48bd62adb0b000a3328478a11d20641b1282
SHIVAM KESHARI <149083540+SHIVAMKESHARI23@users.noreply.github.com>
2026-06-30 11:34:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Iterative Inorder/iterative-inorder.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+
+    void inorder(Node* root, vector<int>& ans) {
+
+        if(root == nullptr)
+            return;
+
+        inorder(root->left, ans);
+        ans.push_back(root->data);
+        inorder(root->right, ans);
+    }
+
+    vector<int> inOrder(Node* root) {
+
+        vector<int> ans;
+
+        inorder(root, ans);
+
+        return ans;
+    }
+};
